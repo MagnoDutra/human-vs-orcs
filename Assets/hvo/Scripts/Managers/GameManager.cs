66c194568c73b7abca14c72a3390b4a3cc0936de
Magno <jmagno.dutra@hotmail.com>
2025-03-03 00:30:31 -0300

    get input positions

--- Assets/hvo/Scripts/Managers/GameManager.cs
+++ Assets/hvo/Scripts/Managers/GameManager.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class GameManager : SingletonManager<GameManager>
 {
+    void Update()
+    {
+        Vector2 inputPosition = Input.touchCount > 0 ? Input.GetTouch(0).position : Input.mousePosition;
 
+        if (Input.GetMouseButtonUp(0) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            DetectClick(inputPosition);
+        }
+    }
+
+    void DetectClick(Vector2 inputPos)
+    {
+        Debug.Log(inputPos);
+    }
 }
