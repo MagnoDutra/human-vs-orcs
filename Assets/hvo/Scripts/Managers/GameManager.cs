550b9ad92aac1c6ac57fb28ecc9c5f6884b1d752
Magno <jmagno.dutra@hotmail.com>
2025-03-03 19:07:14 -0300

    Prevent click while sliding the touch

--- Assets/hvo/Scripts/Managers/GameManager.cs
+++ Assets/hvo/Scripts/Managers/GameManager.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public class GameManager : SingletonManager<GameManager>
 {
+    private Vector2 initialTouchPoisition;
+
     void Update()
     {
         Vector2 inputPosition = Input.touchCount > 0 ? Input.GetTouch(0).position : Input.mousePosition;
 
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            initialTouchPoisition = inputPosition;
+        }
+
         if (Input.GetMouseButtonUp(0) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
-            DetectClick(inputPosition);
+            if (Vector2.Distance(initialTouchPoisition, inputPosition) < 10)
+            {
+                DetectClick(inputPosition);
+            }
         }
     }
 
