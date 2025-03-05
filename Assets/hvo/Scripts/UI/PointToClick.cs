17835ea5ed13188f60af78df0ac6bc605e4c5e66
Magno <jmagno.dutra@hotmail.com>
2025-03-05 13:58:12 -0300

    Animation Curve

--- Assets/hvo/Scripts/UI/PointToClick.cs
+++ Assets/hvo/Scripts/UI/PointToClick.cs
@@ -4,12 +4,24 @@ public class PointToClick : MonoBehaviour
 {
     [SerializeField] private float duration = 1f;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private AnimationCurve scaleCurve;
+
+    private Vector3 initialScale;
 
     private float timer;
+
+    void Start()
+    {
+        initialScale = transform.localScale;
+        transform.localScale *= 0;
+    }
 
     void Update()
     {
         timer += Time.deltaTime;
+
+        float scaleMultiplier = scaleCurve.Evaluate(timer);
+        transform.localScale = initialScale * scaleMultiplier;
 
         if (timer >= duration * 0.9f)
         {
